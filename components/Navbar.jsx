091af1e91bf5e35275ffaec5601596523eea6004
Emir Cokrlija <144956911+emirCo95@users.noreply.github.com>
2024-12-02 18:19:41 -0500

    create Navbar.jsx

---
+++ components/Navbar.jsx
@@ -0,0 +1,14 @@
+import Image from 'next/image';
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <div className="w-full h-[20vh]">
+      <div className="w-full h-full flex items-center justify-center">
+        <Image src="/logo.png" alt="logo" width={400} height={400} />
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
