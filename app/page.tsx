653e5b4b37332042919d21422d5827ec3ab9d2f5
Emir Cokrlija <144956911+emirCo95@users.noreply.github.com>
2024-12-06 16:26:45 -0500

    update Home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,17 @@
+import Image from 'next/image';
+
 export default function Home() {
-  return <div className="w-[65%] h-full">hi</div>;
+  return (
+    <div className="w-full md:w-[65%] h-full">
+      <div className="w-full h-full flex items-center justify-center">
+        <Image
+          className="-z-10"
+          src="/logo.png"
+          alt="logo"
+          width={1000}
+          height={1000}
+        />
+      </div>
+    </div>
+  );
 }
