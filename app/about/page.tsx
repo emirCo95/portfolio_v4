a32efb7b0841e0cf2216d88e09de545dbb122d94
Emir Cokrlija <144956911+emirCo95@users.noreply.github.com>
2024-12-07 19:42:18 -0500

    Update page.tsx

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -19,18 +19,18 @@ const About = () => {
           <p
             className={`${playFair.className} antialiased text-lg max-w-[350px] lg:max-w-[400px] mt-5 text-center`}
           >
-            I am a <span className="font-bold">front-end developer</span> that
-            loves to combine creativity with technical skills to create
-            interactive and visually appealing experiences. I love the challenge
-            of turning complex ideas into intuitive designs that users can
-            engage with effortlessly. Watching a concept come to life on the
-            screen, knowing it can positively impact someone&apos;s experience,
-            is incredibly rewarding. The fast-paced nature of front-end
-            development keeps me motivated, as there is always something new to
-            learn, whether it&apos;s mastering a new framework, improving
-            performance, or exploring cutting-edge design trends. It&apos;s this
-            mix of creativity, problem-solving, and innovation that drives my
-            passion for front-end development.
+            A <span className="font-bold">front-end developer</span> that loves
+            to combine creativity with technical skills to create interactive
+            and visually appealing experiences. I love the challenge of turning
+            complex ideas into intuitive designs that users can engage with
+            effortlessly. Watching a concept come to life on the screen, knowing
+            it can positively impact someone&apos;s experience, is incredibly
+            rewarding. The fast-paced nature of front-end development keeps me
+            motivated, as there is always something new to learn, whether
+            it&apos;s mastering a new framework, improving performance, or
+            exploring cutting-edge design trends. It&apos;s this mix of
+            creativity, problem-solving, and innovation that drives my passion
+            for front-end development.
           </p>
         </div>
       </motion.div>
